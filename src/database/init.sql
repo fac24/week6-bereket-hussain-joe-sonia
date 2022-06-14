e68a3ea7ad3d078ea6db55cf8b074d86f91d2aa5
joe <joe.dev.public@gmail.com>
2022-06-14 15:13:15 +0100

    Cleaned up whitespace

--- src/database/init.sql
+++ src/database/init.sql
@@ -12,14 +12,13 @@ CREATE TABLE products (
 INSERT INTO products (name, description, price) VALUES
     ('Apathy', 'Do you even care?', 5),
     ('Confidence', 'We got it in buckets', 9),
-      ('Esteem', 'nice to have it but it ok if you dont', 3),
-        ('Nostalgia', 'looking back sucks', 3),
-          ('Fulfilment', 'halve a glass full kind of guy', 4),
-          ('Rage', 'nothing like a good dose of rage', 5),
-           ('Frustration', 'life is full of it', 5),
-            ('Smugness', 'useful tool in life', 2),
-            ('Love', 'is overrated', 1),
-            ('Satisfaction', 'an itch you want to scratch', 5);
-
+    ('Esteem', 'nice to have it but it ok if you dont', 3),
+    ('Nostalgia', 'looking back sucks', 3),
+    ('Fulfilment', 'halve a glass full kind of guy', 4),
+    ('Rage', 'nothing like a good dose of rage', 5),
+    ('Frustration', 'life is full of it', 5),
+    ('Smugness', 'useful tool in life', 2),
+    ('Love', 'is overrated', 1),
+    ('Satisfaction', 'an itch you want to scratch', 5);
 
 COMMIT;
