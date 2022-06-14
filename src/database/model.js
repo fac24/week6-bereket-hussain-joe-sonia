3dd16ca0a9b59e69fceadbedce3180a92b662d70
root <sonia.botsarova@gmail.com>
2022-06-14 15:51:52 +0100

    display basic data on main page

--- src/database/model.js
+++ src/database/model.js
@@ -3,8 +3,12 @@ const db = require("./connection.js");
 async function getProducts() {
     const SELECT_PRODUCTS= `
     SELECT id, name, description, price FROM 
-    products
+    products;
     `
     const products = await db.query(SELECT_PRODUCTS);
     return products.rows; 
+}
+
+module.exports = {
+    getProducts
 }
