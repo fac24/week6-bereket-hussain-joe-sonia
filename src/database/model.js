f76ba1c05024e8c3b0ce499627584ab9e20bb69e
root <sonia.botsarova@gmail.com>
2022-06-15 10:18:13 +0100

    select image from products table

--- src/database/model.js
+++ src/database/model.js
@@ -1,14 +1,14 @@
 const db = require("./connection.js");
 
 async function getProducts() {
-    const SELECT_PRODUCTS= `
-    SELECT id, name, description, price FROM 
+  const SELECT_PRODUCTS = `
+    SELECT id, name, description, price, image FROM 
     products;
-    `
-    const products = await db.query(SELECT_PRODUCTS);
-    return products.rows; 
+    `;
+  const products = await db.query(SELECT_PRODUCTS);
+  return products.rows;
 }
 
 module.exports = {
-    getProducts
-}
+  getProducts,
+};
