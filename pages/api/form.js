7c8543b4d62531f0583918b1fad92daa01354e50
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-17 16:11:01 +0100

    clean up

--- pages/api/form.js
+++ pages/api/form.js
@@ -4,22 +4,9 @@ export default function handler(req, res) {
   // Get data submitted in request's body.
   const body = req.body;
 
-  // console.log(`\n\nmy log\n\n`);
-  // console.log(req);
-  // console.log(req.method);
-  // console.log(`\n\nend of my log\n\n`);
-
   if (req.method === "GET") {
     res.status(200).json(basketItems);
   } else if (req.method === "POST") {
-    // console.log("hello world");
-
-    // basketItems.map((item) => {
-    //   if (item.product_id === Number(body.productId)) {
-    //     item.quantity = item.quantity + Number(body.quantity);
-    //   }
-    // });
-
     let updateExistingItem = false;
 
     for (let i = 0; i < basketItems.length; i++) {
