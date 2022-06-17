def2f6e2eda3fb4a777de0e4c9249e1a43f61f9d
joe <joe.dev.public@gmail.com>
2022-06-17 15:25:01 +0100

    Give API route GET and POST handlers

--- pages/api/form.js
+++ pages/api/form.js
@@ -2,23 +2,44 @@ import { basketItems } from "../../data/basketItems";
 
 export default function handler(req, res) {
   // Get data submitted in request's body.
-  //   const body = req.body;
+  const body = req.body;
 
-  // Optional logging to see the responses
-  // in the command line where next.js app is running.
-  //   console.log("body: ", body);
+  // console.log(`\n\nmy log\n\n`);
+  // console.log(req);
+  // console.log(req.method);
+  // console.log(`\n\nend of my log\n\n`);
 
-  //   // Guard clause checks for first and last name,
-  //   // and returns early if they are not found
-  //   if (!body.productId || !body.quantity) {
-  //     // Sends a HTTP bad request error code
-  //     return res
-  //       .status(400)
-  //       .json({ id: body.productId, quantity: body.quantity });
-  //   }
-  // Found the name.
-  // Sends a HTTP success code
-  res.status(200).json(basketItems);
+  if (req.method === "GET") {
+    res.status(200).json(basketItems);
+  } else if (req.method === "POST") {
+    // console.log("hello world");
+
+    // basketItems.map((item) => {
+    //   if (item.product_id === Number(body.productId)) {
+    //     item.quantity = item.quantity + Number(body.quantity);
+    //   }
+    // });
+
+    let updateExistingItem = false;
+
+    for (let i = 0; i < basketItems.length; i++) {
+      if (basketItems[i].product_id === Number(body.productId)) {
+        basketItems[i].quantity =
+          basketItems[i].quantity + Number(body.quantity);
+        updateExistingItem = true;
+        break;
+      }
+    }
+
+    if (updateExistingItem === false) {
+      basketItems.push({
+        product_id: Number(body.productId),
+        quantity: Number(body.quantity),
+      });
+    }
+
+    res.redirect("/basket");
+  }
 }
 
-//{ id: body.productId, quantity: body.quantity }
+//
